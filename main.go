dd5fde2bccfadd24eb86b2c4d60541a726271793
Boris <soskinboris@gmail.com>
2026-09-21 22:10:33 +0300

    home work #1

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"runtime"
+)
 
 func main() {
-	fmt.Println("Hello, Go!")
+	fmt.Println("Пользователь:", os.Getenv("USER"))
+
+	args := os.Args[1:]
+	if len(args) == 0 {
+		fmt.Println("Аргументы CLI: не переданы")
+	} else {
+		fmt.Println("Аргументы CLI:")
+		for i, arg := range args {
+			fmt.Printf("  %d: %s\n", i+1, arg)
+		}
+	}
+
+	fmt.Println("Версия Go:", runtime.Version())
 }
